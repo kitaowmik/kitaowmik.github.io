47ffaa6c34013dfec2d626d80b1c11edc4df0b50
kitaowmik <80123072+kitaowmik@users.noreply.github.com>
2023-08-18 21:18:56 -0700

    updating site

--- name.js
+++ name.js
@@ -1,22 +1,31 @@
 import * as THREE from './build/three.module.js';
-import {FlyControls} from './src/FlyControls.js';
-import { GLTFLoader } from './src/GLTFLoader.js';
-import Stats from './src/stats.module.js';
+// import {OrbitControls} from './src/OrbitControls.js';
+// import { GLTFLoader } from './src/GLTFLoader.js';
+// import Stats from './src/stats.module.js';
 
-let controls, stats, pointLight;
+
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
-const clock = new THREE.Clock();
+
+const mouse = new THREE.Vector2();
+const target = new THREE.Vector2();
+const windowHalf = new THREE.Vector2( window.innerWidth / 2, window.innerHeight / 2 );
+
 
 const starGeo = new THREE.BufferGeometry();
 const starMat = new THREE.PointsMaterial({color: 0xffffff});
 
 const stars = new THREE.Points(starGeo, starMat);
 scene.add(stars);
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
 const starPos = [];
 for (let i = 0; i < 10000; i++) {
@@ -28,56 +37,12 @@ for (let i = 0; i < 10000; i++) {
 
 starGeo.setAttribute('position', new THREE.Float32BufferAttribute(starPos, 3));
 renderer.setClearColor(0x000000);
-renderer.setSize(innerWidth,innerHeight);
+renderer.setSize(window.innerWidth,window.innerHeight);
 renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-camera.position.set(1,0.45,2);
-camera.rotation.set(0,0,-0.2);
+camera.position.set(0,0,0);
 
-
-let planetOne = new THREE.Object3D();
-const loaderOne = new GLTFLoader().load(
-    "./assets/asphalt.glb",
-    function(gltf) {
-    planetOne = gltf.scene;
-    planetOne.position.set(0,0,0);
-    planetOne.rotation.set(0, 0, 0); 
-    planetOne.scale.set(.3, .3, .3);
-    scene.add(planetOne);
-    },
-    undefined,
-    function(error) {
-    console.error(error);
-    }
-);
-
-
-/*et spaceDust = new THREE.Object3D();
-const loaderThree = new GLTFLoader().load(
-    "./assets/asteroids.glb",
-    function(gltfthree) {
-    spaceDust = gltfthree.scene;
-    spaceDust.position.set(-0.9,0,.5);
-    spaceDust.rotation.set(0, 0, 0); 
-    spaceDust.scale.set(0.2, .2, .2);
-    scene.add(spaceDust);
-    },
-    undefined,
-    function(error) {
-    console.error(error);
-    }
-);*/
-
-// var mouseSpeed = 0.0001;
-
-// document.onmousemove = function (e) {
-//   var centerX = window.innerWidth * 0.5;
-//   //var centerY = window.innerHeight * 0.5;
-
-//   camera.position.x = (e.clientX - centerX) * mouseSpeed;
-//   camera.position.y = (e.clientY - centerY) * mouseSpeed;
-// };
 
 
 const light = new THREE.DirectionalLight( 0xFFFFFF, 50.5);
@@ -88,20 +53,37 @@ scene.add(light);
 const ambientLight = new THREE.AmbientLight(0xD66868, 0.8);
 scene.add(ambientLight);
  let t = 0;
+
+ document.addEventListener( 'mousemove', onMouseMove, false );
+ window.addEventListener('resize', onWindowResize, false)
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+
+function onMouseMove( event ) {
+
+	mouse.x = ( event.clientX - windowHalf.x );
+	mouse.y = ( event.clientY - windowHalf.x );
+
+}
 function animate() {
 
-    t += 0.05;
-    const timer = 0.0003 * Date.now();
-
-  
-    planetOne.position.y = Math.cos( timer ) * 0.2;
-    planetOne.position.z = Math.sin( timer ) * 0.2;
+    target.x = ( 1 - mouse.x ) * 0.0002;
+    target.y = ( 1 - mouse.y ) * 0.0002;
     
+    camera.rotation.x += 0.05 * ( target.y - camera.rotation.x );
+    camera.rotation.y += 0.05 * ( target.x - camera.rotation.y );
 
-    
-    requestAnimationFrame(animate);
+    requestAnimationFrame( animate );
 
     
+    
+
+
     renderer.render( scene, camera );
    
 }
